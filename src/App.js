104b500f0b1a578e4928d376c39346027af1b1f5
Ankita196 <muskanmakde1999@gmail.com>
2021-08-26 12:49:14 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>E commerce</h1>
      
     </div>
   );
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,15 @@
 import React from "react"
 import Grid from '@material-ui/core/Grid';
 
+
+const  products=[
+  {id:1,name:'shoue', discription:'running shoes'}
+]
+
 const Products=()=>{
-  return
+  return(
+    <Grid container justify='center' spacing={4}>   </Grid>
+  )
 }
 
 export default Products
