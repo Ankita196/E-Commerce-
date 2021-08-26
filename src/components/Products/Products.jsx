536c9821bafd2ae5b75be06fdceae3c754846977
Ankita196 <muskanmakde1999@gmail.com>
2021-08-26 13:01:07 +0530

    1 file modified

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -10,7 +10,8 @@ const Products = () => {
   return <Grid container justify="center" spacing={4} >
 
     {products.map((product)=>(
-      <Grid item key ={product.id} xs={12} sm={6} md={4} lg={3}> </Grid>
+      <Grid item key ={product.id} xs={12} sm={6} md={4} lg={3}>
+        <Product /> </Grid>
     ))}
     </Grid>;
 };
