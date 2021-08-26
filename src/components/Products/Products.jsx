86cff39f44f069ef8ec8f14d3cc9211d9defdd05
Ankita196 <muskanmakde1999@gmail.com>
2021-08-26 12:55:26 +0530

    1 file modified

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,24 +1,18 @@
-import React from "react"
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
+const products = [
+  { id: 1, name: 'shoue', discription: 'running shoes' },
+  { id: 2, name: 'book', discription: 'soft book' }
+];
 
-const  products=[
-  {id:1,name:'shoue', discription:'running shoes'},
-  {id:2,name:'book', discription:'soft book'}
-  
+const Products = () => {
+  return <Grid container justify="center" spacing={4} >
 
+    {products.map((product)=>(
+      <Grid item key ={product.id} xs={12} sm={6} md={4} lg={3}> </Grid>
+    ))}
+    </Grid>;
+};
 
-
-
-
-]
-
-const Products=()=>{
-  return(
-    <Grid container justify='center' spacing={4}>   </Grid>
-  )
-}
-
-export default Products
-
-
+export default Products;
