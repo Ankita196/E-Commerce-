45539994caea99134d11bdcef57d9690a69a7028
Ankita196 <muskanmakde1999@gmail.com>
2021-08-26 13:13:18 +0530

    1 file modified

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,21 @@
-import React from "react"
-import {}
+import React from 'react';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton
+} from '@material-ui/core';
+import {AddShoppingCart} from "@material-ui/icons"
 
-const Product=()=>{
-  return(
-    <div></div>
-  )
-}
+const Product = () => {
+  return (
+   <Card className ={classes.root}>
+<CarddMedia className={classes.media} image='' title={product.name}/>
+<CardContent></CardContent>
+   </Card>
+
+
+  );
+};
