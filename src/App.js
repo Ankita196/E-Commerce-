0e8f667ad1c9e36e0ba32819a14b695960e51254
Ankita196 <muskanmakde1999@gmail.com>
2021-08-28 19:04:05 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const App =()=> {
-  return (
-    <div>
-      <h1>
-        product
-      </h1>
-    </div>
+  const [product,setProduct]=useState({})
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+  }, []);
+  
+    
+if(product){
+  return( 
+  <h1>{product.title}
+  </h1>
+
   );
 }
 
