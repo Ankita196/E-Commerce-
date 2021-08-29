20c8dda5cec86d2223c9c16a77796d4e5932fe59
Ankita196 <muskanmakde1999@gmail.com>
2021-08-29 20:32:40 +0530

    1 file modified

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -4,88 +4,74 @@ import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     maxWidth: 345,
   },
   media: {
-   
-    paddingTop: '56.25%', // 16:9
-   height:"50%",
-   width:"50%",
-  marginLeft:"auto",
-  marginRight:"auto"
-
-    
+    paddingTop: '100%', // 16:9
+    width: '100%',
+    height: 150,
+    justifyContent: 'center',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 50
   },
   expand: {
     transform: 'rotate(0deg)',
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+      duration: theme.transitions.duration.shortest
+    })
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: 'rotate(180deg)'
   },
   avatar: {
-    backgroundColor: red[500],
-  },
+    backgroundColor: red[500]
+  }
 }));
 
-const Products =()=> {
+const Products = () => {
   const classes = useStyles();
-  const [products,setProducts]=useState([])
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
-    .then((res)=>res.json())
-    .then((json)=>setProducts(json))
+      .then(res => res.json())
+      .then(json => setProducts(json));
   }, []);
-  
-    
 
-  return( 
-    
-  <div>
-    {products.map((product,index)=>(
-      <div>
-       <Card className={classes.root}>
-      <CardHeader
-       
-       
-        title={product.title}
-       
-      />
-      <CardMedia
-        className={classes.media}
-        image={product.image}
-        title={product.title}
+  return (
+    <div >
+
+      {products.map((product, index) => (
         
-      />
-    
-          <Typography >Price:{product.price}</Typography>
-       
-          
-          
-       
-     
-    </Card>
-      
-      </div>
-    ))} </div>
+          <Card className={classes.root}>
+            <CardHeader title={product.title} />
+            <CardMedia
+              className={classes.media}
+              image={product.image}
+              title={product.title}
+            />
 
+            <Typography>Price:{product.price}</Typography>
+          </Card>
+       
+      ))}{' '}
+    </div>
   );
-}
+};
 
-export default Products
+export default Products;
