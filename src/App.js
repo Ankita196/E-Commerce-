b26f13d04c5a6645b57f096aab7c2e395996795f
Ankita196 <muskanmakde1999@gmail.com>
2021-08-29 18:55:58 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const App =()=> {
-  const [products,setProducts]=useState([])
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-    .then((res)=>res.json())
-    .then((json)=>setProducts(json))
-  }, []);
-  
+ 
     
 
   return( 
   <div>
-    {products.map((product,index)=>(
-      <div>
-        <h1>{product.title}</h1>
-        <img src={product.image} />
-        <h1>{product.price}</h1>
-      </div>
-    ))} </div>
+    </div>
+
 
   );
 }
