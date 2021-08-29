4a4cb22ccb7a6be0e0572d0cb13797e799bd880e
Ankita196 <muskanmakde1999@gmail.com>
2021-08-29 18:55:18 +0530

    1 file added

---
+++ src/components/products.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+const Products =()=> {
+  const [products,setProducts]=useState([])
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then((res)=>res.json())
+    .then((json)=>setProducts(json))
+  }, []);
+  
+    
+
+  return( 
+  <div>
+    {products.map((product,index)=>(
+      <div>
+        <h1>{product.title}</h1>
+        <img src={product.image} />
+        <h1>{product.price}</h1>
+      </div>
+    ))} </div>
+
+  );
+}
+
+export default Products
