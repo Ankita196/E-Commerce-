07a295666bed23258c07f6d274a6c326c4d9a92e
Ankita196 <muskanmakde1999@gmail.com>
2021-08-30 16:12:39 +0530

    1 file modified

--- src/components/Products/product.jsx
+++ src/components/Products/product.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import { Link }  from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
-import CardMedia from '@material-ui/core/CardMedia';
+
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
+
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -31,19 +30,20 @@ const Product = ({ title, image, price}) => {
   return (
     <div>
       <Card className={classes.root}>
-       
-     <img src={image} style={{height:"50%",width:"50%",display:"block"}} className={classes.media}/>
+       <Link>
+     <img src={image} style={{height:"50%",width:"50%",display:"block"}} className={classes.media}/> </Link>
         {/* <CardMedia className={classes.media} image={image} title={title} /> */}
         <CardContent>
           <Grid style={{height:90}}>
         <Typography style={{ textAlign: 'center',}}>{title}</Typography>
         </Grid>
+      
         <Typography style={{textAlign: 'center'}}>RS: {price}</Typography>
         <Button variant="contained" color="secondary" style={{display:"block", marginRight:"auto", marginLeft:"auto",}}>
         ADD TO CART
       </Button>
         </CardContent>
-        
+       
        
       </Card>
     </div>
