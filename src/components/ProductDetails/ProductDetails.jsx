b4020def4139f0155fcfe8a0ca464a4fc18dae00
Ankita196 <muskanmakde1999@gmail.com>
2021-08-30 15:56:29 +0530

    1 file modified

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,1 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+
+const ProductDetails = () => {
+  const classes = useStyles();
+  const [product, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/')
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }, []);
+
+  return (
+<div>
+      
+      <div>
+        <Grid container spacing={7}>
+       
+            <Grid
+              item
+              xs={12}
+              sm={3}
+              style={{marginLeft:"auto", alignItems: 'center' }}
+            >
+              
+              <h1> {product.title}</h1>
+               <img src={product.image} />
+              <h5> {product.price}</h5>
+             
+         
+        </Grid>
+      </div>
+    </div>
+  );
+  }
