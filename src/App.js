26da3ef25c6bcba9bd7561fa705354bcf86efa18
Ankita196 <muskanmakde1999@gmail.com>
2021-09-05 17:12:23 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
-import Products from "./components/Products/Products"
-import ProductDetails from "./components/ProductDetails/ProductDetails"
-import {BrowserRouter ,Route,Switch} from "react-router-dom"
-import Navigation from "./components/Navigation/Navigation"
+import Products from './components/Products/Products';
+import ProductDetails from './components/ProductDetails/ProductDetails';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
 
-const App =()=> {
- 
-     
-
-  return( 
-  <div>
-
-
- <BrowserRouter>
- <Navigation/>
- <Switch>
- <Route exact path="/"><Products /></Route>
-<Route path="/products/:id"><ProductDetails /></Route>
-</Switch>
- </BrowserRouter>
-    
-</div>
-
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path="/">
+            <Products />
+          </Route>
+          <Route path="/products/:id" component={ProductDetails} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
-  }
+};
 
-export default App
+export default App;
