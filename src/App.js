88594ca9232f29831ae5986f3a908e4a2e780686
Ankita196 <muskanmakde1999@gmail.com>
2021-09-07 12:50:04 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './style.css';
 import Products from './components/Products/Products';
 import ProductDetails from './components/ProductDetails/ProductDetails';
